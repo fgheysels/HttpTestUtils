e254a314b6996f6ee93b1e0bf8ec998fababcffa
Frederik Gheysels <frederik.gheysels@codit.eu>
2020-04-30 14:36:29 +0200

    Initial commit of HttpClientMock

---
+++ src/HttpTestUtils/HttpTestUtils/HttpClientMock.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpTestUtils
+{
+    public static class HttpClientMock
+    {
+        public static HttpClient SetupHttpClientWithJsonResponse<T>(HttpStatusCode responseStatusCode, T responseBody)
+        {
+            var messageHandler = new TestHttpMessageHandler( _ => Task.FromResult(new HttpResponseMessage(responseStatusCode) { Content = new StringContent(JsonConvert.SerializeObject(responseBody), Encoding.UTF8, "application/json") }));
+
+            return new HttpClient(messageHandler);
+        }
+
+        private class TestHttpMessageHandler : HttpMessageHandler
+        {
+            private readonly Func<HttpRequestMessage, Task<HttpResponseMessage>> _createResponseFunction;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="TestHttpMessageHandler"/> class.
+            /// </summary>
+            public TestHttpMessageHandler(Func<HttpRequestMessage, Task<HttpResponseMessage>> createResponse)
+            {
+                _createResponseFunction = createResponse;
+            }
+
+            /// <summary>Send an HTTP request as an asynchronous operation.</summary>
+            /// <param name="request">The HTTP request message to send.</param>
+            /// <param name="cancellationToken">The cancellation token to cancel operation.</param>
+            /// <returns>The task object representing the asynchronous operation.</returns>
+            /// <exception cref="T:System.ArgumentNullException">The <paramref name="request">request</paramref> was null.</exception>
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                return await _createResponseFunction(request);
+            }
+        }
+    }
+}
